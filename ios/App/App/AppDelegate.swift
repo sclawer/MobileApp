c67b0011f2d6b2e7c982c36ea9ac5b003062f32e
Steven Lawer <sclawer@gmail.com>
2022-04-17 18:31:13 +1000

    Diagnostics for Auth

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Capacitor
+import FirebaseCore
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -18,14 +18,10 @@
           >Sign In with Google</ion-button
         >
         <strong v-if="loggedInUser">{{ loggedInUser.displayName }}</strong>
+        <strong v-else>Not logged in</strong>
+        <p>Is native app {{ platform }}</p>
         <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
+          {{ userEmail }}
         </p>
         <ion-button @click="signOut" expand="block">Sign out</ion-button>
       </div>
@@ -44,16 +40,12 @@ import {
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 import { ref } from "vue";
+import { isPlatform } from "@ionic/vue";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCIMp93ApXrejda5-SQovAv7fQeOWEPs_0",
+  apiKey: "AIzaSyCIMp93ApXrejda5 - SQovAv7fQeOWEPs_0",
   authDomain: "dgsa-test.firebaseapp.com",
   projectId: "dgsa-test",
   storageBucket: "dgsa-test.appspot.com",
@@ -62,14 +54,19 @@ const firebaseConfig = {
   measurementId: "G-QMWNNRVPRP",
 };
 initializeApp(firebaseConfig);
-
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const auth = getAuth();
-
-// Initialize Firebase
-
+// const auth = getAuth();
 import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
+// fire addListener place 1 - can see in logfile of IOS but not firing on auth state change
+// FirebaseAuthentication.addListener("authStateChange", (change) => {
+//   console.log("AuthStateChanged fired");
+//   console.log("authStateChange result", change);
+// });
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithCredential,
+  // onAuthStateChanged,
+} from "firebase/auth";
 
 export default defineComponent({
   name: "HomePage",
@@ -82,20 +79,26 @@ export default defineComponent({
     IonButton,
   },
   setup() {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        const uid = user.uid;
-        console.log("User is signed in", uid);
-        loggedInUser.value = user;
-        // ...
-      } else {
-        // User is signed out
-        // ...
-      }
-    });
+    // onAuthStateChanged(auth, (user) => {
+    //   if (user) {
+    //     // User is signed in, see docs for a list of available properties
+    //     // https://firebase.google.com/docs/reference/js/firebase.User
+    //     const uid = user.uid;
+    //     console.log("User is signed in", uid);
+    //     loggedInUser.value = user;
+    //     // ...
+    //   } else {
+    //     console.log("signout");
+    //     // User is signed out
+    //     // ...
+    //   }
+    // });
+    // function authChange(user) {
+    //   console.log("authChange", user);
+    // }
+    const platform = isPlatform("capacitor");
     const loggedInUser = ref(null);
+    const userEmail = ref("");
     const getCurrentUser = async () => {
       const result = await FirebaseAuthentication.getCurrentUser();
       return result.user;
@@ -106,14 +109,52 @@ export default defineComponent({
       return result.token;
     };
     const signInWithGoogle = async () => {
-      await FirebaseAuthentication.signInWithGoogle();
+      // fire addListener place 2 - can see in logfile of IOS but not firing on auth state change
+      // FirebaseAuthentication.addListener("authStateChange", (change) => {
+      //   console.log("AuthStateChanged fired");
+      //   console.log("authStateChange result", change);
+      // });
+      // 1. Create credentials on the native layer
+      const result = await FirebaseAuthentication.signInWithGoogle();
+      if (platform) {
+        console.log("Native Platform detected");
+        // fire addListener place 3 - can see in logfile of IOS but not firing on auth state change
+        FirebaseAuthentication.addListener("authStateChange", (change) => {
+          console.log("AuthStateChanged fired");
+          console.log("authStateChange result", change);
+        });
+        userEmail.value = result.user.displayName;
+        const credential = result.credential;
+        console.log("credential", credential);
+        const auth = getAuth();
+        // console.log("currentUser");
+        // const credUser = await signInWithCredential(auth, credential);
+        // console.log("credUser", credUser);
+        const FBuser = auth.currentUser.getIdTokenResult();
+        console.log("CurrUser", FBuser);
+        // console.log(result.user.displayName);
+      } else {
+        console.log("Non IOS platform");
+        // 2. Sign in on the web layer using the id token
+        const credential = GoogleAuthProvider.credential(
+          result.credential?.idToken
+        );
+        const auth = getAuth();
+        const user = await signInWithCredential(auth, credential);
+        const FBuser = await auth.currentUser.getIdTokenResult();
+        console.log("claims", FBuser.claims);
+        // console.log(user.user.email);
+        userEmail.value = user.user.email;
+      }
     };
     const signOut = async () => {
       await FirebaseAuthentication.signOut();
       loggedInUser.value = null;
     };
     return {
+      platform,
       loggedInUser,
+      userEmail,
       getCurrentUser,
       getIdToken,
       signInWithGoogle,
